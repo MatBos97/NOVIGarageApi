54dbc752f9550d70682e8598ecdedbb92648348b
matbos <mathijs.bos@prodrive-technologies.com>
2022-09-26 09:53:06 +0200

    Update MethodSecurityConfig.java
    Added the rolehierarchy to the default method security expression handler

--- src/main/java/mathijs/bos/NOVIGarageApi/Security/MethodSecurityConfig.java
+++ src/main/java/mathijs/bos/NOVIGarageApi/Security/MethodSecurityConfig.java
@@ -2,15 +2,23 @@ package mathijs.bos.NOVIGarageApi.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+import javax.annotation.Resource;
 
 @Configuration
 @EnableGlobalMethodSecurity(
         securedEnabled = true
 )
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+    @Resource
+    private RoleHierarchy roleHierarchy;
 
     @Bean
     public RoleHierarchyImpl roleHierarchy(){
@@ -19,5 +27,12 @@ public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
                 "ROLE_ADMIN > ROLE_RECEPTIONIST and ROLE_MECHANIC and ROLE_CASHIER"
         );
         return roleHierarchy;
+    }
+
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = (DefaultMethodSecurityExpressionHandler) super.createExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+        return expressionHandler;
     }
 }
